171babce29f9a367a3539ae63a2a13a60337e3a5
서건희 <seogunhee4@gmail.com>
2021-12-05 00:49:23 +0900

    [FIX]

--- TrackOn/app/src/main/java/com/example/trackon/model/UserModel.kt
+++ TrackOn/app/src/main/java/com/example/trackon/model/UserModel.kt
@@ -14,7 +14,7 @@ import retrofit2.Response
 import retrofit2.Retrofit
 import retrofit2.converter.gson.GsonConverterFactory
 
-class UserModel() {
+class sUserModel() {
 
     private val userService: UserService
 
